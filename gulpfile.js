961d22466529b153c3f2b87cedd3f06cb497472c
Victor <victor.ananias8@gmail.com>
2018-08-20 13:21:03 -0300

    DONE

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const phpspec = require('gulp-phpspec');
+const notify = require('gulp-notify');
+
+gulp.task('test', function() {
+    gulp.src('spec/**/*.php')
+        .pipe(phpspec('', { 'verbose': 'v', notify: true, clear: true }))
+        .on('error', notify.onError({
+            title: "Crap",
+            message: "Your tests failed.",
+            icon: __dirname + '/fail.jpg'
+        }))
+        .pipe(notify({
+            title: "Success",
+            message: "All tests have returned green!"
+        }));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(['spec/**/*.php', 'src/**/*.php'], ['test']);
+});
+
+gulp.task('default', ['test', 'watch']);
